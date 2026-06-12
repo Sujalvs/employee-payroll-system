e31ece2a151f7e1c0f944d3a2ae9d00842042c6a
SUJAL <sujal@SUJALs-MacBook-Pro.local>
2026-06-12 17:27:29 +0530

    Fix Railway port

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
